320df48fc85b0998c0af0896bb6ef071252a592d
Claudio Zera <claudiozera44@gmail.com>
2025-10-28 11:42:53 +0000

    CORS, pushed allowed origins to appsettings to avoid hard-coding URLs

--- Program.cs
+++ Program.cs
@@ -9,11 +9,13 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient<IEmployeeService, EmployeeService>();
 builder.Services.AddSingleton<INoteService, NoteService>();
 
+var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>() ?? Array.Empty<string>();
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularApp", policy =>
     {
-        policy.WithOrigins("http://localhost:4200")
+        policy.WithOrigins(allowedOrigins)
               .SetIsOriginAllowed(origin => true)
               .AllowAnyMethod()
               .AllowAnyHeader();
